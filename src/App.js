5d8a5f255e70578aee6ca78737a0b439f8cee6c9
Maicol Avila <maavilav@correo.udistrital.edu.cm>
2020-05-04 13:31:37 -0500

    FirstCommit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav> <h1 className="tittle">G<span className="Tdiferent">A</span>C</h1></nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
