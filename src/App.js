aa6084146b40605836dceebd6e126b736f1074c4
Maicol Avila <maavilav@correo.udistrital.edu.cm>
2020-05-05 10:31:07 -0500

    secondCommit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Publicidad(props){
+ return <div className="publicidad" width="100%" > 
+          <img className="publicidad"src="" alt="" width="100%"/>
+        </div>
+}
+const publicidad = <Publicidad />;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <nav> <h1 className="tittle">G<span className="Tdiferent">A</span>C</h1></nav>
+        <nav> 
+          <ul>
+            <li className="tittle">G<span className="Tdiferent">A</span>C</li>
+            <input type="search" className="search"/>
+            <li>Categoría</li>
+          </ul>
+        </nav>
+        
+        <Publicidad />
       </header>
     </div>
   );
